417774e98a78e8801f0d4174c3dae4f11da54d19
João Martins <john_victor100@hotmail.com>
2022-05-03 15:39:31 -0300

    Author data with DTO

--- src/main/java/com/jvictorweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jvictorweb/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.jvictorweb.workshopmongo.domain.Post;
 import com.jvictorweb.workshopmongo.domain.User;
+import com.jvictorweb.workshopmongo.dto.AuthorDTO;
 import com.jvictorweb.workshopmongo.repositories.PostRepository;
 import com.jvictorweb.workshopmongo.repositories.UserRepository;
 
@@ -36,10 +37,12 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob ));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
