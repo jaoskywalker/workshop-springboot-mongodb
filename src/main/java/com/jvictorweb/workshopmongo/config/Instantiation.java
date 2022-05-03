756de1eb9ec24c935ceef8fc106548ae04b5f8ad
João Martins <john_victor100@hotmail.com>
2022-05-03 16:45:27 -0300

    Simple query using query methods

--- src/main/java/com/jvictorweb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/jvictorweb/workshopmongo/config/Instantiation.java
@@ -41,7 +41,7 @@ public class Instantiation implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(maria, alex, bob ));
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("25/04/2019"), "Partiu viagem", "Vou viajar para Curitiba. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("25/04/2019"), "Bom dia", "Vou viajar para Curitiba. Abraços!", new AuthorDTO(maria));
 		
 		CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex)); 
 		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
